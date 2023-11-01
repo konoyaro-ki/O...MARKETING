b859416f682b2038332710b9f964179ffc74a5f5
konoyaro_ki <chepel1997@gmail.com>
2023-11-01 20:39:52 +0600

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-$(function(){
-  var mixer = mixitup('.portfolio__content');
+$(function () {
 
   $('.slider-blog__inner').slick({
     dots: true,
-    prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
   });
+
+  $('.menu__btn').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
+
 });
