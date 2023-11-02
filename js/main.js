f554dc917d26fbc7f5a887743b36f8415575ffa4
konoyaro_ki <chepel1997@gmail.com>
2023-11-02 23:09:13 +0600

    next adaptive

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
 
   $('.menu__btn').on('click', function () {
